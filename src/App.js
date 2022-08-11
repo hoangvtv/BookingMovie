004be8a12a3911a73c25c415f003defa5f2847f8
hoangvtv <phamtanhoangvtv@gmail.com>
2022-08-11 16:06:31 +0700

    chore: config routing

--- src/App.js
+++ src/App.js
@@ -2,8 +2,18 @@ import logo from "./logo.svg";
 import "./App.css";
 import "./App.css";
 
+import { createBrowserHistory } from "history";
+import { Route, Routes } from "react-router-dom";
+import HomeTempale from "./templates/HomeTemplate/HomeTempale";
+
+export const history = createBrowserHistory();
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Routes history={history}>
+      <Route path="/" element={<HomeTempale />}></Route>
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/templates/HomeTemplate/HomeTempale.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+
+export default function HomeTempale() {
+  return (
+    <Fragment>
+      <h1 className="bg-black h-10 text-white">Đây là header homepage</h1>
+
+      <footer className="bg-black h-10 text-white">
+        Đây là footer homepage
+      </footer>
+    </Fragment>
+  );
+}
